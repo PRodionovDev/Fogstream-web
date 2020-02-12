7873df484f0ea0b501738185707ce790f32e168e
THE JOKER <pavel11_06@mail.ru>
2020-02-12 16:02:06 +0800

    read news

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\News;
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $model = News::orderBy('created_at', 'asc')->paginate(10);
+
+        return view('welcome')->with([
+            'model' => $model
+        ]);
+    }
+}
